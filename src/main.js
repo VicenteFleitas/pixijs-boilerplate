3a8b6dbd639329a747c075fc7c55aa3b83c1a6a6
VicenteFleitas <vicente1fleitas1@gmail.com>
2025-01-22 20:24:38 -0300

    feat: initial settings

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import { Application } from "pixi.js";
 
 (async () => {
   const app = new Application();
-  await app.init();
+  await app.init({
+    // width: 1024,
+    // height: 512,
+    resizeTo: window,
+    backgroundColor: "#2c3e50",
+    // antialias: true,
+  });
+  app.canvas.style.position = "absolute";
+  document.body.style.margin = 0;
   document.body.appendChild(app.canvas);
 })();
