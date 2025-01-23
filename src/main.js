d52877db5d71774ebbb857f76341f9f323cf62f5
VicenteFleitas <vicente1fleitas1@gmail.com>
2025-01-22 21:50:47 -0300

    feat: rect, text and sprites

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { Application, Graphics, Text, TextStyle } from "pixi.js";
+import {
+  Application,
+  Graphics,
+  Text,
+  TextStyle,
+  Sprite,
+  Assets,
+} from "pixi.js";
 
 (async () => {
   const app = new Application();
@@ -34,6 +41,18 @@ import { Application, Graphics, Text, TextStyle } from "pixi.js";
     style: style,
   });
   app.stage.addChild(text);
+
+  const texture = await Assets.load("/img/SpriteSheet.png");
+  //   const sprite = Sprite.from(texture);
+  const sprite = new Sprite(texture); // 128, 224
+  sprite.x = 512;
+  sprite.y = 256;
+  sprite.scale.x = 2;
+  sprite.scale.y = 2;
+  sprite.anchor.x = 0.5;
+  sprite.anchor.y = 0.5;
+  sprite.rotation = Math.PI / 4;
+  app.stage.addChild(sprite);
 
   document.body.appendChild(app.canvas);
 })();
